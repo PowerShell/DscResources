50d42fa91257ff628f69e89f429942dc1b5265c8
Daniel Scott-Raynsford <plagueho@gmail.com>
2016-01-26 14:39:03 +1300

    Remove V2 from Region Header

--- Tests.Template/unit_template.ps1
+++ Tests.Template/unit_template.ps1
@@ -17,7 +17,7 @@ $Global:DSCModuleName      = 'x<ModuleName>' # Example xNetworking
 $Global:DSCResourceName    = 'MSFT_x<ResourceName>' # Example MSFT_xFirewall
 # /TODO
 
-#region HEADER (V2)
+#region HEADER
 [String] $moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))
 if ( (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
      (-not (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
