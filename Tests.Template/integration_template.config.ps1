eeb9b67df12543baf41aee7576701e26b0b915a8
Johan Ljunggren <johan.ljunggren@shamn.se>
2018-05-30 07:08:19 +0200

    Fix review comments at r2

--- Tests.Template/integration_template.config.ps1
+++ Tests.Template/integration_template.config.ps1
@@ -19,13 +19,13 @@
 #endregion
 
 <#
-    TODO: (Optional) If appropriate this configuration hash table
+    TODO: (Optional) If appropriate, this configuration hash table
     can be moved from here and into the integration test file.
-    For example if there are several configurations which all
+    For example, if there are several configurations which all
     need different configuration properties, it might be easier
     to have one ConfigurationData-block per configuration test
-    then one big ConfigurationData-block here.
-    It can also be moved if it is easier to read the tests when
+    than one big ConfigurationData-block here.
+    It may also be moved if it is easier to read the tests when
     the ConfigurationData-block is in the integration test file.
     The reason for it being here is that it is easier to read
     the configuration when the ConfigurationData-block is in this
@@ -44,7 +44,7 @@ $ConfigurationData = @{
 
 <#
     .SYNOPSIS
-        TODO: Add a short but descriptive text what this configuration does.
+        TODO: Add a short but clear description of what this configuration does.
         (e.g. Enables the TCP port for Remote Desktop Connection on the profile Public.)
 #>
 # TODO: Modify ResourceName (e.g. MSFT_Firewall_EnableRemoteDesktopConnection_Config).
